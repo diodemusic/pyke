e93fa1c3014bd81685d2fafb0f9e64b21d7dd10f
diodemusic <diodemusicuk@gmail.com>
2026-02-08 01:17:34 +0000

    Add ddragon readme

---
+++ src/pyke/ddragon/__init__.py
@@ -0,0 +1,7 @@
+"""
+Data Dragon module.
+
+This module provides access to League of Legends static data through Data Dragon.
+It includes classes and functions for retrieving champion information, items, runes, maps,
+and other game assets.
+"""
